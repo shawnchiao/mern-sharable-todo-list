50e2cec8e2174f6baefa72a4dd6dc0bf91975855
Shawn Chiao <shawn1763@gmail.com>
2022-10-01 17:31:34 +0930

    add new todo connect to database

--- src/todolists/pages/AddTodoList.js
+++ src/todolists/pages/AddTodoList.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useEffect } from "react";
+import React, { useReducer, useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import Button from "@mui/material/Button";
@@ -16,11 +16,15 @@ import ChairTwoToneIcon from "@mui/icons-material/ChairTwoTone";
 import SendIcon from "@mui/icons-material/Send";
 import Divider from "@mui/material/Divider";
 import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
 
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import CustomSwitch from "../../shared/components/FormElements/CustomSwitch";
+import useHttpClient from "../../shared/hooks/httpHook";
+import { AuthContext } from "../../shared/context/authContext";
 
-
-const TypesOfTodoList = [
+const Types = [
   { type: "Shopping", avatar: <ShoppingCartTwoToneIcon /> },
   { type: "Work", avatar: <WorkHistoryTwoToneIcon /> },
   { type: "Everyday", avatar: <ChairTwoToneIcon /> },
@@ -28,91 +32,149 @@ const TypesOfTodoList = [
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'setTypeOfTodo':
-      return { typeOfTodo: action.payload, setting: state.setting };
-    case 'setIsPublic':
-      return { ...state, setting: { isPublic: action.payload, isEditable: state.setting.isEditable} }
-    case 'setIsEditable':
-      return { ...state, setting: { isPublic: state.setting.isPublic , isEditable: action.payload } }
+    case "setTypeOfTodo":
+      return { ...state, typeOfTodo: action.payload, title: action.payload };
+    case "setIsPublic":
+      return {
+        ...state,
+        setting: {
+          isPublic: action.payload,
+          isEditable: state.setting.isEditable,
+        },
+      };
+    case "setIsEditable":
+      return {
+        ...state,
+        setting: {
+          isPublic: state.setting.isPublic,
+          isEditable: action.payload,
+        },
+      };
+    case "setTitle":
+      return { ...state, title: action.payload };
     default:
-        return state;
+      return state;
   }
 };
 
-
 export default function SimpleDialog(props) {
-
-  const [state, dispatch] = useReducer(reducer, { typeOfTodo: '', setting: { isPublic: false, isEditable: true} })
-
-
+  const auth = useContext(AuthContext);
+  const { error, isLoading, sendRequest, clearError } = useHttpClient();
+  const [state, dispatch] = useReducer(reducer, {
+    typeOfTodo: "",
+    setting: { isPublic: false, isEditable: true },
+    title: "",
+  });
   const navigate = useNavigate();
-  const [open, setOpen] = React.useState(false);
-  const [selectedValue, setSelectedValue] = React.useState(TypesOfTodoList[1]);
+  const [open, setOpen] = useState(true);
 
   const handleClose = (selectedValue) => {
     setOpen(false);
-    setSelectedValue(selectedValue);
+    navigate("/");
   };
   useEffect(() => {
-    setOpen(true);
-  }, []);
+    if (error) {
+      setOpen(false);
+    }
+  }, [error]);
 
   const handleListItemClick = (value) => {
-
     dispatch({ type: "setTypeOfTodo", payload: value });
   };
 
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    try {
+      await sendRequest(
+        `${process.env.REACT_APP_BACKEND_URL}/todolists`,
+        "POST",
+        JSON.stringify({
+          title: state.title,
+          type: state.typeOfTodo,
+          isPublic: state.setting.isPublic,
+          isEditable: state.setting.isEditable,
+          creator: auth.userId,
+        }),
+        {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + auth.token,
+        }
+      );
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-  console.log(JSON.stringify(state))
+  console.log(JSON.stringify(state));
   return (
-    <Dialog onClose={handleClose} open={open} fullWidth="true">
-      <div className="typeChoices">
-        <DialogTitle>Choose a to-do list</DialogTitle>
-        <List sx={{ pt: 0 }}>
-          {TypesOfTodoList.map((each) => (
-            <ListItem
-              button
-              onClick={() => handleListItemClick(each.type)}
-              key={each.type}
-              style={{ background: state.typeOfTodo === each.type && "#FD841F" }}
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && <LoadingSpinner asOverlay />}
+      <Dialog onClose={handleClose} open={open} fullWidth="true">
+        <form onSubmit={submitHandler}>
+          <div className="typeChoices">
+            <DialogTitle>Choose a to-do list</DialogTitle>
+            <List sx={{ pt: 0 }}>
+              {Types.map((each) => (
+                <ListItem
+                  button
+                  onClick={() => handleListItemClick(each.type)}
+                  key={each.type}
+                  style={{
+                    background: state.typeOfTodo === each.type && "#FD841F",
+                  }}
+                >
+                  <ListItemAvatar>
+                    <Avatar
+                      sx={{ bgcolor: blueGrey[50], color: blueGrey[700] }}
+                    >
+                      {each.avatar}
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText primary={each.type} />
+                </ListItem>
+              ))}
+            </List>
+            <TextField
+              style={{ top: "1rem" }}
+              id="outlined-basic"
+              label="Title"
+              variant="outlined"
+              value={state.title}
+              onChange={(e) => {
+                dispatch({ type: "setTitle", payload: e.target.value });
+              }}
+            />
+          </div>
+          <Stack direction="column" spacing={1.5}>
+            <Divider />
+            <CustomSwitch
+              style={{ margin: "auto auto 10px" }}
+              label={true ? "Allow other users to view" : "Only you can view "}
+              onChange={(v) => dispatch({ type: "setIsPublic", payload: v })}
+              checked={state.setting.isPublic}
+            />
+            <CustomSwitch
+              style={{ margin: "auto auto 20px" }}
+              label={true ? "Allow other users to edit" : "Only you can edit "}
+              onChange={(v) => dispatch({ type: "setIsEditable", payload: v })}
+              checked={state.setting.isEditable}
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              endIcon={<SendIcon />}
+              style={{ width: "17em", margin: "auto" }}
+              size="medium"
             >
-              <ListItemAvatar>
-                <Avatar sx={{ bgcolor: blueGrey[50], color: blueGrey[700] }}>
-                  {each.avatar}
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary={each.type} />
-            </ListItem>
-          ))}
-        </List>
-      </div>
-      <Stack direction="column" spacing={1.5}>
-        <Divider />
-        <CustomSwitch
-          style={{ margin: "auto auto 10px" }}
-          label={true? "Allow other users to view" : "Only you can view "}
-          onChange={(v)=> dispatch({ type: "setIsPublic", payload: v })}
-          checked={state.setting.isPublic}
-
-        />
-        <CustomSwitch
-          style={{ margin: "auto auto 20px" }}
-          label={true ? "Allow other users to edit" : "Only you can edit "}
-          onChange={(v)=> dispatch({ type: "setIsEditable", payload: v }) }
-          checked={state.setting.isEditable}
-        />
-        <Button
-          variant="contained"
-          endIcon={<SendIcon />}
-          style={{ width: "17em", margin: "auto" }}
-          size="medium"
-          onClick={() => navigate("/todoLists/t1")}
-        >
-          Confirm
-        </Button>
-        <Divider />
-      </Stack>
-    </Dialog>
+              Confirm
+            </Button>
+            <Divider />
+          </Stack>
+        </form>
+      </Dialog>
+    </>
   );
 }
 
